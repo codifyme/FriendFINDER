511a941bc59c8fb8f836e2d5060dcbb993bcf2dd
Angel Cochachez <acochachez@gmail.com>
2019-12-16 22:04:55 -0800

    Heroku deploy1

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,53 +2,52 @@
 var friendsArray = [
   {
     name: 'siko',
-    photo: '',
+    photo: 'https://images.app.goo.gl/aF37mfRAcXYFTKw76',
     scores: ['1', '3', '6', '8', '2', '4', '7', '2', '1', '6']
   },
   {
     name: 'mota',
-    photo: '',
+    photo: 'https://images.app.goo.gl/n45pbfDteaKdDU5bA',
     scores: ['1', '2', '6', '8', '2', '9', '7', '2', '1', '6']
   },
   {
     name: 'mia',
-    photo: '',
+    photo: 'https://images.app.goo.gl/W329p5gqTw2JjJy66',
     scores: ['8', '3', '6', '8', '2', '1', '7', '1', '1', '6']
   },
   {
     name: 'loka',
-    photo: '',
+    photo: 'https://images.app.goo.gl/7Stj14PP8kxmb4oK8',
     scores: ['9', '3', '6', '8', '2', '4', '7', '2', '1', '6']
   },
   {
     name: 'thor',
-    photo: '',
+    photo: 'https://images.app.goo.gl/YTWrataUe3umQUTp8',
     scores: ['4', '1', '1', '8', '2', '4', '7', '2', '1', '6']
   },
   {
-
-    name: 'vido',
-    photo: '',
+    name: 'lasi',
+    photo: 'https://images.app.goo.gl/TnhGfcBg5awW2vPR7',
     scores: ['3', '2', '2', '8', '2', '4', '7', '2', '1', '6']
   },
   {
     name: 'comanche',
-    photo: '',
+    photo: 'https://images.app.goo.gl/LiVqbW4Pj3WaW6VU8',
     scores: ['7', '3', '1', '8', '2', '4', '7', '1', '1', '6']
   },
   {
     name: 'lisa',
-    photo: '',
+    photo: 'https://images.app.goo.gl/1hqB89rM1UauypBZ9',
     scores: ['5', '3', '6', '8', '6', '8', '7', '2', '1', '6']
   },
   {
     name: 'rocotico',
-    photo: '',
+    photo: 'https://images.app.goo.gl/Lb8njxGhp1DdrLRC9',
     scores: ['6', '3', '6', '6', '2', '4', '7', '2', '1', '6']
   },
   {
     name: 'suler',
-    photo: '',
+    photo: 'https://images.app.goo.gl/sKypT8pX75SCLQ498',
     scores: ['2', '2', '6', '8', '2', '4', '7', '2', '1', '6']
   }
 ];
